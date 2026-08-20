a20032b39936519850662e201440a49d4294edf2
brick-24 <raghavsethi.in@gmail.com>
2026-08-20 18:09:32 +0530

    add http test

--- tests/test_http.py
+++ tests/test_http.py
@@ -1,31 +1,5 @@
 import requests
-from function.forecast import get_weekly_weather
-from function.news import fetch_news
-from function.weather import get_weather
 
-def test_weather_http_check():
-    weather = get_weather("Jaipur")
-
-    assert weather["description"]
-    assert weather["temp"] != ""
-    assert weather["humidity"] != ""
-    assert weather["sunrise"]
-    assert weather["sunset"]
-
-
-def test_forecast_http_check():
-    forecast = get_weekly_weather("Jaipur")
-
-    assert len(forecast) == 7
-    assert forecast[0]["date"]
-    assert forecast[0]["description"]
-    assert isinstance(forecast[0]["high"], int)
-    assert isinstance(forecast[0]["low"], int)
-    assert forecast[0]["rain"] is not None
-
-
-def test_news_rss_http_check():
-    headlines = fetch_news(limit=1)
-
-    assert len(headlines) == 1
-    assert headlines[0].strip()
+def test_http():
+    response = requests.get("https://google.com/")
+    assert response.status_code == 200
